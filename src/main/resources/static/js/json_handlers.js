2ff7ee6e55cd4104d67577090162fd514d4f7cd0
Miłosz Wilczyński <mwilcz08@gmail.com>
2020-01-18 18:06:21 +0100

    added more

--- src/main/resources/static/js/json_handlers.js
+++ src/main/resources/static/js/json_handlers.js
@@ -385,3 +385,398 @@ function sendWahaczeJSON(tuleje, pukanie, szarpie) {
     }
 }
 
+//pompa hamulcowa
+
+function prepareTarczeHamulcoweJSON(){
+    var przegrzewaja = parseInt(document.querySelector('input[name="0"]:checked').value);
+
+    sendTarczeHamulcoweJSON(przegrzewaja);
+}
+
+function sendTarczeHamulcoweJSON(przegrzewaja) {
+    const xmlhttp = new XMLHttpRequest();
+    const url="/hamulce/tarcze";
+    xmlhttp.open("POST", url, true);
+    xmlhttp.setRequestHeader("Content-Type", "application/json");
+    xmlhttp.send(JSON.stringify({
+        "tarczeHamulcowe":{
+            "CzySiePrzegrzewaja": przegrzewaja,
+            "CzySaUszkodzone": 0,
+            "wiadomosc": ""
+        }
+    }));
+
+    xmlhttp.onreadystatechange = function(){
+        if(xmlhttp.status == 200){
+            var object = (xmlhttp.responseText);
+            document.getElementById('containerDiv').style.display = "none";
+            if(object.toString().length < 4){
+                //nie pokazuj nic
+            }else {
+                document.getElementById("containerDiv").innerHTML = object.toString();
+                document.getElementById('containerDiv').style.display = "block";
+
+                //hide after 3s
+                setTimeout(function () {document.getElementById('containerDiv').style.display='none'}, 3000); return false
+            }
+        }else{
+            console.log("nie dziala");
+        }
+    }
+}
+
+//tarcze klocki
+
+function prepareTarczeKlockiJSON(){
+    var uszkodzone = parseInt(document.querySelector('input[name="0"]:checked').value);
+    var przegrzewaja = parseInt(document.querySelector('input[name="1"]:checked').value);
+
+    sendTarczeKlockiJSON(uszkodzone, przegrzewaja);
+}
+
+function sendTarczeKlockiJSON(uszkodzone, przegrzewaja) {
+    const xmlhttp = new XMLHttpRequest();
+    const url="/hamulce/tarczeklocki";
+    xmlhttp.open("POST", url, true);
+    xmlhttp.setRequestHeader("Content-Type", "application/json");
+    xmlhttp.send(JSON.stringify({
+        "tarczeHamulcowe":{
+            "CzySiePrzegrzewaja": 0,
+            "CzySaUszkodzone": uszkodzone,
+            "wiadomosc": ""
+        },
+        "klockiHamulcowe":{
+            "czyZuzyte": 0,
+            "czySiePrzegrzewaja": przegrzewaja,
+            "wiadomosc": "",
+        }
+    }));
+
+    xmlhttp.onreadystatechange = function(){
+        if(xmlhttp.status == 200){
+            var object = (xmlhttp.responseText);
+            document.getElementById('containerDiv').style.display = "none";
+            if(object.toString().length < 4){
+                //nie pokazuj nic
+            }else {
+                document.getElementById("containerDiv").innerHTML = object.toString();
+                document.getElementById('containerDiv').style.display = "block";
+
+                //hide after 3s
+                setTimeout(function () {document.getElementById('containerDiv').style.display='none'}, 3000); return false
+            }
+        }else{
+            console.log("nie dziala");
+        }
+    }
+}
+
+//opony
+
+function prepareOponyJSON(){
+    var bieznikZuzyty = parseInt(document.querySelector('input[name="0"]:checked').value);
+    var cisnieniePrawidlowe = parseInt(document.querySelector('input[name="1"]:checked').value);
+    var przebite = parseInt(document.querySelector('input[name="2"]:checked').value);
+    var traciSterownosc = parseInt(document.querySelector('input[name="3"]:checked').value);
+    var drgania = parseInt(document.querySelector('input[name="4"]:checked').value);
+
+    sendOponyJSON(bieznikZuzyty, cisnieniePrawidlowe, przebite, traciSterownosc, drgania);
+}
+
+function sendOponyJSON(bieznikZuzyty, cisnieniePrawidlowe, przebite, traciSterownosc, drgania) {
+    const xmlhttp = new XMLHttpRequest();
+    const url="/opony";
+    xmlhttp.open("POST", url, true);
+    xmlhttp.setRequestHeader("Content-Type", "application/json");
+    xmlhttp.send(JSON.stringify({
+        "opony":{
+            "czyBieznikZuzyty": bieznikZuzyty,
+            "czyCisnieniePrawidlowe": cisnieniePrawidlowe,
+            "czyPrzebite": przebite,
+            "czySamochodTraciSterownosc:": traciSterownosc,
+            "czyDrganiaPodczasJazdy": drgania,
+            "wiadomosc": ""
+        }
+    }));
+
+    xmlhttp.onreadystatechange = function(){
+        if(xmlhttp.status == 200){
+            var object = (xmlhttp.responseText);
+            document.getElementById('containerDiv').style.display = "none";
+            if(object.toString().length < 4){
+                //nie pokazuj nic
+            }else {
+                document.getElementById("containerDiv").innerHTML = object.toString();
+                document.getElementById('containerDiv').style.display = "block";
+
+                //hide after 3s
+                setTimeout(function () {document.getElementById('containerDiv').style.display='none'}, 3000); return false
+            }
+        }else{
+            console.log("nie dziala");
+        }
+    }
+}
+
+//skrzynia biegów automatyczna
+
+function prepareAutomatycznaJSON(){
+    var rozlaczanieNapedu = parseInt(document.querySelector('input[name="0"]:checked').value);
+    var brakBieguWstecznego = parseInt(document.querySelector('input[name="1"]:checked').value);
+    var brakWysokich = parseInt(document.querySelector('input[name="2"]:checked').value);
+
+    sendAutomatycznaJSON(rozlaczanieNapedu, brakBieguWstecznego, brakWysokich);
+}
+
+function sendAutomatycznaJSON(rozlaczanieNapedu, brakBieguWstecznego, brakWysokich) {
+    const xmlhttp = new XMLHttpRequest();
+    const url="/skrzynia/automatyczna";
+    xmlhttp.open("POST", url, true);
+    xmlhttp.setRequestHeader("Content-Type", "application/json");
+    xmlhttp.send(JSON.stringify({
+        "automatyczna":{
+            "wiadomosc": "",
+            "czyRozlaczanieNapedu": rozlaczanieNapedu,
+            "czyBrakBieguWstecznego": brakBieguWstecznego,
+            "czyBrakWysokichBiegow:": brakWysokich,
+        }
+    }));
+
+    xmlhttp.onreadystatechange = function(){
+        if(xmlhttp.status == 200){
+            var object = (xmlhttp.responseText);
+            document.getElementById('containerDiv').style.display = "none";
+            if(object.toString().length < 4){
+                //nie pokazuj nic
+            }else {
+                document.getElementById("containerDiv").innerHTML = object.toString();
+                document.getElementById('containerDiv').style.display = "block";
+
+                //hide after 3s
+                setTimeout(function () {document.getElementById('containerDiv').style.display='none'}, 3000); return false
+            }
+        }else{
+            console.log("nie dziala");
+        }
+    }
+}
+
+//układ kierowniczy
+
+function prepareUkladKierowniczyJSON(){
+    var szarpanie = parseInt(document.querySelector('input[name="0"]:checked').value);
+    var luz = parseInt(document.querySelector('input[name="1"]:checked').value);
+    var drganie = parseInt(document.querySelector('input[name="2"]:checked').value);
+    var wspomaganie = parseInt(document.querySelector('input[name="3"]:checked').value);
+    var wspomaganiev2 = parseInt(document.querySelector('input[name="4"]:checked').value);
+    var wycie = parseInt(document.querySelector('input[name="5"]:checked').value);
+    var utrudnione = parseInt(document.querySelector('input[name="6"]:checked').value);
+    var gwizdy = parseInt(document.querySelector('input[name="7"]:checked').value);
+
+    sendUkladKierowniczyJSON(szarpanie, luz, drganie, wspomaganiem, wspomaganiev2, wycie, utrudnione, gwizdy);
+}
+
+function sendUkladKierowniczyJSON(szarpanie, luz, drganie, wspomaganiem, wspomaganiev2, wycie, utrudnione, gwizdy) {
+    const xmlhttp = new XMLHttpRequest();
+    const url="/ukladkierowniczy";
+    xmlhttp.open("POST", url, true);
+    xmlhttp.setRequestHeader("Content-Type", "application/json");
+    xmlhttp.send(JSON.stringify({
+        "ukladKierowniczy":{
+            "czySzarpaniePodczasSkrecania": szarpanie,
+            "czyLuzWKolachPodczasJazdy": luz,
+            "czyDrganieKierownicyPodczasPostoju": drganie,
+            "czyWspomaganieDziala": wspomaganiem,
+            "wspomaganieRazDzialaRazNie": wspomaganiev2,
+            "wiadomosc": "",
+            "wyciePompyWTrakcieJazdy": wycie,
+            "czyUtrudnioneSkrecanie": utrudnione,
+            "czyGwizdyPompyWTrakcieJazdy": gwizdy,
+        }
+    }));
+
+    xmlhttp.onreadystatechange = function(){
+        if(xmlhttp.status == 200){
+            var object = (xmlhttp.responseText);
+            document.getElementById('containerDiv').style.display = "none";
+            if(object.toString().length < 4){
+                //nie pokazuj nic
+            }else {
+                document.getElementById("containerDiv").innerHTML = object.toString();
+                document.getElementById('containerDiv').style.display = "block";
+
+                //hide after 3s
+                setTimeout(function () {document.getElementById('containerDiv').style.display='none'}, 3000); return false
+            }
+        }else{
+            console.log("nie dziala");
+        }
+    }
+}
+
+//układ wydech - katalizator
+
+function prepareKatalizatorJSON(){
+    var grzechotanie = parseInt(document.querySelector('input[name="0"]:checked').value);
+    var problemUruchomienie = parseInt(document.querySelector('input[name="1"]:checked').value);
+    var przyduszanie = parseInt(document.querySelector('input[name="2"]:checked').value);
+
+    sendKatalizatorJSON(grzechotanie, problemUruchomienie, przyduszanie);
+}
+
+function sendKatalizatorJSON(grzechotanie, problemUruchomienie, przyduszanie) {
+    const xmlhttp = new XMLHttpRequest();
+    const url="/wydech/katalizator";
+    xmlhttp.open("POST", url, true);
+    xmlhttp.setRequestHeader("Content-Type", "application/json");
+    xmlhttp.send(JSON.stringify({
+        "katalizator":{
+            "wiadomosc": "",
+            "czyGrzechotaniePodczasPracy": grzechotanie,
+            "czyProblemyZUruchomieniemSilnika": problemUruchomienie,
+            "czyPrzyduszanieSilnika": przyduszanie
+        }
+    }));
+
+    xmlhttp.onreadystatechange = function(){
+        if(xmlhttp.status == 200){
+            var object = (xmlhttp.responseText);
+            document.getElementById('containerDiv').style.display = "none";
+            if(object.toString().length < 4){
+                //nie pokazuj nic
+            }else {
+                document.getElementById("containerDiv").innerHTML = object.toString();
+                document.getElementById('containerDiv').style.display = "block";
+
+                //hide after 3s
+                setTimeout(function () {document.getElementById('containerDiv').style.display='none'}, 3000); return false
+            }
+        }else{
+            console.log("nie dziala");
+        }
+    }
+}
+
+//układ wydech - kolektor
+
+function prepareKolektorJSON(){
+    var halas = parseInt(document.querySelector('input[name="0"]:checked').value);
+
+    sendKolektorJSON(halas);
+}
+
+function sendKolektorJSON(halas) {
+    const xmlhttp = new XMLHttpRequest();
+    const url="/wydech/kolektor";
+    xmlhttp.open("POST", url, true);
+    xmlhttp.setRequestHeader("Content-Type", "application/json");
+    xmlhttp.send(JSON.stringify({
+        "kolektor":{
+            "wiadomosc": "",
+            "czyHalasPodczasPracy": halas
+        }
+    }));
+
+    xmlhttp.onreadystatechange = function(){
+        if(xmlhttp.status == 200){
+            var object = (xmlhttp.responseText);
+            document.getElementById('containerDiv').style.display = "none";
+            if(object.toString().length < 4){
+                //nie pokazuj nic
+            }else {
+                document.getElementById("containerDiv").innerHTML = object.toString();
+                document.getElementById('containerDiv').style.display = "block";
+
+                //hide after 3s
+                setTimeout(function () {document.getElementById('containerDiv').style.display='none'}, 3000); return false
+            }
+        }else{
+            console.log("nie dziala");
+        }
+    }
+}
+
+//układ wydech - sonda
+
+function prepareSondaLambdaJSON(){
+    var spadekMocy = parseInt(document.querySelector('input[name="0"]:checked').value);
+    var wzrostZuzycia = parseInt(document.querySelector('input[name="1"]:checked').value);
+
+    sendSondaLambdaJSON(spadekMocy, wzrostZuzycia);
+}
+
+function sendSondaLambdaJSON(spadekMocy, wzrostZuzycia) {
+    const xmlhttp = new XMLHttpRequest();
+    const url="/wydech/sondalambda";
+    xmlhttp.open("POST", url, true);
+    xmlhttp.setRequestHeader("Content-Type", "application/json");
+    xmlhttp.send(JSON.stringify({
+        "sondaLambda":{
+            "wiadomosc": "",
+            "czySpadekMocySilnika": spadekMocy,
+            "czyWzrostZuzyciaPaliwa": wzrostZuzycia,
+        }
+    }));
+
+    xmlhttp.onreadystatechange = function(){
+        if(xmlhttp.status == 200){
+            var object = (xmlhttp.responseText);
+            document.getElementById('containerDiv').style.display = "none";
+            if(object.toString().length < 4){
+                //nie pokazuj nic
+            }else {
+                document.getElementById("containerDiv").innerHTML = object.toString();
+                document.getElementById('containerDiv').style.display = "block";
+
+                //hide after 3s
+                setTimeout(function () {document.getElementById('containerDiv').style.display='none'}, 3000); return false
+            }
+        }else{
+            console.log("nie dziala");
+        }
+    }
+}
+
+//układ wydech - wydech
+
+function prepareWydechJSON(){
+    var bialy = parseInt(document.querySelector('input[name="0"]:checked').value);
+    var niebieski = parseInt(document.querySelector('input[name="1"]:checked').value);
+    var czarny = parseInt(document.querySelector('input[name="2"]:checked').value);
+
+    sendWydechJSON(bialy, niebieski, czarny);
+}
+
+function sendWydechJSON(bialy, niebieski, czarny) {
+    const xmlhttp = new XMLHttpRequest();
+    const url="/wydech/wydech";
+    xmlhttp.open("POST", url, true);
+    xmlhttp.setRequestHeader("Content-Type", "application/json");
+    xmlhttp.send(JSON.stringify({
+        "wydech":{
+            "wiadomosc": "",
+            "czyDuzoBialegoDymu": bialy,
+            "czyKolorDymuNiebieski": niebieski,
+            "czyKolorDymuCzarny": czarny
+        }
+    }));
+
+    xmlhttp.onreadystatechange = function(){
+        if(xmlhttp.status == 200){
+            var object = (xmlhttp.responseText);
+            document.getElementById('containerDiv').style.display = "none";
+            if(object.toString().length < 4){
+                //nie pokazuj nic
+            }else {
+                document.getElementById("containerDiv").innerHTML = object.toString();
+                document.getElementById('containerDiv').style.display = "block";
+
+                //hide after 3s
+                setTimeout(function () {document.getElementById('containerDiv').style.display='none'}, 3000); return false
+            }
+        }else{
+            console.log("nie dziala");
+        }
+    }
+}
+
