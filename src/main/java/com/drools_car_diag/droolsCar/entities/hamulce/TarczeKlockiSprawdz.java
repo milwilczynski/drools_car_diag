893cd0d17138489f7e40c6b5deb64e43b9d8be86
Miłosz Wilczyński <mwilcz08@gmail.com>
2020-01-18 17:08:55 +0100

    funny merge

---
+++ src/main/java/com/drools_car_diag/droolsCar/entities/hamulce/TarczeKlockiSprawdz.java
@@ -0,0 +1,22 @@
+package com.drools_car_diag.droolsCar.entities.hamulce;
+
+public class TarczeKlockiSprawdz {
+    KlockiHamulcowe klockiHamulcowe;
+    TarczeHamulcowe tarczeHamulcowe;
+
+    public TarczeHamulcowe getTarczeHamulcowe() {
+        return tarczeHamulcowe;
+    }
+
+    public void setTarczeHamulcowe(TarczeHamulcowe tarczeHamulcowe) {
+        this.tarczeHamulcowe = tarczeHamulcowe;
+    }
+
+    public KlockiHamulcowe getKlockiHamulcowe() {
+        return klockiHamulcowe;
+    }
+
+    public void setKlockiHamulcowe(KlockiHamulcowe klockiHamulcowe) {
+        this.klockiHamulcowe = klockiHamulcowe;
+    }
+}
